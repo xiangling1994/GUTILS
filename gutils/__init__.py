0438d9de1daae209c9ee7b83329f33da6e7be1f5
Kyle Wilcox <kyle@axiomdatascience.com>
2017-08-03 18:01:04 -0400

    Release 2.0.0

--- gutils/__init__.py
+++ gutils/__init__.py
@@ -16,7 +16,7 @@ from scipy.signal import boxcar, convolve
 import logging
 L = logging.getLogger(__name__)
 
-__version__ = "1.2.6"
+__version__ = "2.0.0"
 
 
 def boxcar_smooth_dataset(dataset, window_size):
