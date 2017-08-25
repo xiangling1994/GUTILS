aea660243a3bc6914912753a3b98905448bf784b
Kyle Wilcox <kyle@axiomdatascience.com>
2017-08-25 14:27:32 -0400

    Change doctr deploy key and options

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,5 +5,5 @@ cd docs
 conda install --file requirements.txt
 sphinx-apidoc -M -f -o api ../gutils ../gutils/tests
 make html
-doctr deploy --built-docs=_site/html .
+doctr deploy --deploy-branch-name gh-pages --built-docs _site/html .
 cd ..
