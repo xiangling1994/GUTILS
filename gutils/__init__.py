b5922109f9ed752639fda785db16498c472575d6
Kyle Wilcox <kyle@axiomdatascience.com>
2017-10-30 14:37:39 -0400

    Only try to get uv data if the original variables are present

--- gutils/__init__.py
+++ gutils/__init__.py
@@ -176,15 +176,16 @@ def get_uv_data(profile):
     u = np.nan
     v = np.nan
 
-    uvslice = (~profile.u_orig.isnull()) & (~profile.v_orig.isnull())
-    uv_index = profile[uvslice].index[:2]
-    if uv_index.size != 0:
-        uv_index = uv_index[-1]
-        t = profile.t.loc[uv_index].to_pydatetime()
-        x = profile.x.loc[uv_index]
-        y = profile.y.loc[uv_index]
-        u = profile.u_orig.loc[uv_index]
-        v = profile.v_orig.loc[uv_index]
+    if 'u_orig' in profile.columns and 'v_orig' in profile.columns:
+        uvslice = (~profile.u_orig.isnull()) & (~profile.v_orig.isnull())
+        uv_index = profile[uvslice].index[:2]
+        if uv_index.size != 0:
+            uv_index = uv_index[-1]
+            t = profile.t.loc[uv_index].to_pydatetime()
+            x = profile.x.loc[uv_index]
+            y = profile.y.loc[uv_index]
+            u = profile.u_orig.loc[uv_index]
+            v = profile.v_orig.loc[uv_index]
 
     tuv = namedtuple('UV_Data', ['t', 'x', 'y', 'u', 'v'])
     return tuv(t=t, x=x, y=y, u=u, v=v)
