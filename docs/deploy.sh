d4167311101abcdc30b7b99993dab902e3c58e29
Kyle Wilcox <kyle@axiomdatascience.com>
2017-10-31 11:44:38 -0400

    Improve doctr setup

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -5,5 +5,5 @@ cd docs
 conda install --file requirements.txt
 sphinx-apidoc -M -f -o api ../gutils ../gutils/tests
 make html
-doctr deploy --deploy-branch-name gh-pages --built-docs _site/html .
 cd ..
+doctr deploy . --built-docs docs/_site/html
